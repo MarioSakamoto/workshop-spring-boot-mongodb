d922158dfa71bdb9dfee69e9dce8aae0fbf564f7
Mario Sakamoto <mariosakamoto.dev@gmail.com>
2022-11-04 16:56:31 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/mariosakamoto/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.mariosakamoto.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mariosakamoto.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
