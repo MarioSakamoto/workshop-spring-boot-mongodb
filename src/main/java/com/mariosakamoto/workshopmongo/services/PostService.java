3b7174ac76578a08d7cce8ae27925e44ea5a8bbf
Mario Sakamoto <mariosakamoto.dev@gmail.com>
2022-11-04 20:41:01 -0300

    Obtendo um post por id

---
+++ src/main/java/com/mariosakamoto/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.mariosakamoto.workshopmongo.services;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mariosakamoto.workshopmongo.domain.Post;
+import com.mariosakamoto.workshopmongo.repository.PostRepository;
+import com.mariosakamoto.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
