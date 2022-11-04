8c58e9c7c64ebd2a4bcee49a4c086f93f10a957d
Mario Sakamoto <mariosakamoto.dev@gmail.com>
2022-11-04 18:08:46 -0300

    Endpont para retornar os posts de um usuário

--- src/main/java/com/mariosakamoto/workshopmongo/resources/UserResource.java
+++ src/main/java/com/mariosakamoto/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.mariosakamoto.workshopmongo.domain.Post;
 import com.mariosakamoto.workshopmongo.domain.User;
 import com.mariosakamoto.workshopmongo.dto.UserDTO;
 import com.mariosakamoto.workshopmongo.services.UserService;
@@ -58,4 +59,9 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value="/{id}/ posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
